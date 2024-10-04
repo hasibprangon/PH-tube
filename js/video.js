48755e916f152febd8af2ce4584f75cb66564497
hasibprangon <hasibulhossainp09@gmail.com>
2024-10-04 22:46:03 +0600

    adding style and modified the card according to api

--- js/video.js
+++ js/video.js
@@ -13,24 +13,29 @@ const displayVideos = (videos) => {
         const videoCard = document.createElement(`div`);
         videoCard.classList = `card card-compact`
         videoCard.innerHTML = `
-     <figure>
-         <img
+     <figure class = "h-[200px] relative">
+         <img class = "w-full h-full object-cover"
              src=${video.thumbnail}
              alt="Shoes" />
+             <span class="absolute right-2 bottom-2 bg-black text-white rounded p-3">${video.others.posted_date}</span>
     </figure>
-  <div class="card-body">
-        <img class="rounded-full w-10 inline-block" src=${video.authors[0].profile_picture} />
-        <h2 class="card-title">${video.title}</h2>
-        <p>${video.authors[0].profile_name}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="px-0 py-2 flex gap-2">
+       <div>
+            <img class="rounded-full w-8 h-8" src=${video.authors[0].profile_picture} />
+       </div>
+        <div>
+            <h2 class="card-title">${video.title}</h2>
+            <div class ="flex gap-2 items-center ">
+                <p class="font-semibold">${video.authors[0].profile_name} </p>
+                ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" />` : ``}
+            </div>
+            <p class = "text-sm">${video.others.views} views</p>
+        </div>
   </div>
     `;
 
-    videoContainer.appendChild(videoCard)
+        videoContainer.appendChild(videoCard)
     });
 }
 
 loadVideos();
-
