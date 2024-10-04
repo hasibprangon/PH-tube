0d29f7be0fd67908e486bbf665506b84ac8601a7
hasibprangon <hasibulhossainp09@gmail.com>
2024-10-04 20:29:21 +0600

    getting the catagory Button From API and Show them in a centered position

---
+++ js/video.js
@@ -0,0 +1,21 @@
+const loadCategories = () => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(err => console.error(`An error found`, err))
+};
+
+const displayCategories = (categories) => {
+    const btnContainer = document.getElementById(`categoryBtnContainer`)
+    categories.forEach(element => {
+        console.log(element);
+
+        const categoryBtn = document.createElement(`button`);
+        categoryBtn.classList= `btn`;
+        categoryBtn.innerText = element.category;
+        btnContainer.appendChild(categoryBtn)
+    });
+}
+
+loadCategories()
